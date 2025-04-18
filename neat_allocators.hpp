57a2bddaa89d7b9e136f83cf49316ec6cadd9d05
Pim Pieters <pim@pieters.tv>
2025-04-18 16:03:48 +0200

    neat_allocators: add bump allocator

--- neat_allocators.hpp
+++ neat_allocators.hpp
@@ -2,16 +2,20 @@
 #define NEAT_ALLOCATORS_HPP_
 
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
-#include <vector>
 
 #ifndef NEAT_ALLOCATORS_MALLOC
 #define NEAT_ALLOCATORS_MALLOC std::malloc
 #endif  // NEAT_ALLOCATORS_MALLOC
 
-#ifndef NEAT_ARENA_FREE
-#define NEAT_ARENA_FREE std::free
-#endif  // NEAT_ARENA_FREE
+#ifndef NEAT_ALLOCATORS_REALLOC
+#define NEAT_ALLOCATORS_REALLOC std::realloc
+#endif  // NEAT_ALLOCATORS_REALLOC
+
+#ifndef NEAT_ALLOCATORS_FREE
+#define NEAT_ALLOCATORS_FREE std::free
+#endif  // NEAT_ALLOCATORS_FREE
 
 namespace neat::allocators {
 
@@ -29,6 +33,30 @@ class arena {
 
     T*   allocate();
     bool failure() const;
+};
+
+class bump {
+   private:
+    struct block {
+        uint8_t*    data;
+        uint8_t*    current;
+        std::size_t remaining;
+    };
+
+    block*      _blocks;
+    std::size_t _block_count;
+    std::size_t _block_capacity;
+    std::size_t _block_size;
+
+    void* add_block();
+
+   public:
+    bump(std::size_t block_size = 4096);
+    ~bump();
+
+    void*                    allocate(std::size_t size);
+    template <typename T> T* allocate();
+    std::size_t              block_count() const;
 };
 
 }  // namespace neat::allocators
@@ -52,7 +80,7 @@ neat::allocators::arena<T>::arena(std::size_t count) {
 template <typename T>
 neat::allocators::arena<T>::arena::~arena() {
     if (_begin != nullptr) {
-        NEAT_ARENA_FREE(_begin);
+        NEAT_ALLOCATORS_FREE(_begin);
     }
 }
 
@@ -77,5 +105,78 @@ bool neat::allocators::arena<T>::arena ::failure() const {
 }
 
 #pragma endregion arena implementations
+
+#pragma region bump implementations
+inline neat::allocators::bump::bump(std::size_t size) {
+    _blocks         = nullptr;
+    _block_count    = 0;
+    _block_capacity = 0;
+    _block_size     = size;
+}
+
+inline neat::allocators::bump::~bump() {
+    for (size_t i = 0; i < _block_count; i++) {
+        NEAT_ALLOCATORS_FREE(_blocks[i].data);
+    }
+    NEAT_ALLOCATORS_FREE(_blocks);
+}
+
+inline void* neat::allocators::bump::allocate(std::size_t size) {
+    if (size > _block_size)
+        return nullptr;
+
+    for (size_t i = 0; i < _block_count; i++) {
+        if (_blocks[i].remaining >= size) {
+            void* ptr = (void*)_blocks[i].current;
+            _blocks[i].current += size / sizeof(uint8_t);
+            _blocks[i].remaining -= size;
+            return ptr;
+        }
+    }
+
+    block* new_block = (block*)add_block();
+    if (!new_block)
+        return nullptr;
+    void* ptr = new_block->current;
+    new_block->current += size / sizeof(uint8_t);
+    new_block->remaining -= size;
+    return ptr;
+}
+
+template <typename T>
+T* neat::allocators::bump::allocate() {
+    return (T*)allocate(sizeof(T));
+}
+
+inline void* neat::allocators::bump::add_block() {
+    // Add new block capacity
+    while (_block_count >= _block_capacity) {
+        size_t new_capacity = _block_capacity == 0 ? 1 : (_block_capacity * 2ull);
+        block* new_blocks   = (block*)NEAT_ALLOCATORS_REALLOC(_blocks, sizeof(block) * new_capacity);
+        if (new_blocks == nullptr) {
+            return nullptr;
+        } else {
+            _block_capacity = new_capacity;
+            _blocks         = new_blocks;
+        }
+    }
+
+    uint8_t* data = (uint8_t*)NEAT_ALLOCATORS_MALLOC(_block_size);
+    if (!data) {
+        return nullptr;
+    }
+    block* new_block     = &_blocks[_block_count];
+    new_block->data      = data;
+    new_block->current   = data;
+    new_block->remaining = _block_size;
+    _block_count++;
+    return new_block;
+}
+
+inline std::size_t neat::allocators::bump::block_count() const {
+    return _block_count;
+}
+
+#pragma endregion bump implementations
 
 #endif  // NEAT_ALLOCATORS_HPP_
--- tests/tests_allocators.cpp
+++ tests/tests_allocators.cpp
@@ -1,7 +1,7 @@
 #include "../neat_allocators.hpp"
 #include "../neat_test.hpp"
 
-void test_small_ints(void) {
+void test_arena_small_ints(void) {
     neat::allocators::arena<int> arena(4);
 
     int* a = arena.allocate();
@@ -23,8 +23,46 @@ void test_small_ints(void) {
     NEAT_TEST_ASSERT(arena.failure());
 }
 
+void test_bump_small(void) {
+    neat::allocators::bump bump1(5);
+
+    int* a = bump1.allocate<int>();
+    int* b = bump1.allocate<int>();
+    int* c = bump1.allocate<int>();
+    int* d = bump1.allocate<int>();
+    int* e = bump1.allocate<int>();
+
+    NEAT_TEST_ASSERT(a != nullptr);
+    NEAT_TEST_ASSERT(b != nullptr);
+    NEAT_TEST_ASSERT(c != nullptr);
+    NEAT_TEST_ASSERT(d != nullptr);
+    NEAT_TEST_ASSERT(e != nullptr);
+    NEAT_TEST_ASSERT(bump1.block_count() == 5);
+
+    neat::allocators::bump bump2(3);
+
+    int* f = bump2.allocate<int>();
+    int* g = bump2.allocate<int>();
+    int* h = bump2.allocate<int>();
+
+    NEAT_TEST_ASSERT(f == nullptr);
+    NEAT_TEST_ASSERT(g == nullptr);
+    NEAT_TEST_ASSERT(h == nullptr);
+    NEAT_TEST_ASSERT(bump2.block_count() == 0);
+
+    char* i = bump2.allocate<char>();
+    char* j = bump2.allocate<char>();
+    char* k = bump2.allocate<char>();
+
+    NEAT_TEST_ASSERT(i != nullptr);
+    NEAT_TEST_ASSERT(j != nullptr);
+    NEAT_TEST_ASSERT(k != nullptr);
+    NEAT_TEST_ASSERT(bump2.block_count() == 1);
+}
+
 int main() {
-    NEAT_TEST_RUN(test_small_ints);
+    NEAT_TEST_RUN(test_arena_small_ints);
+    NEAT_TEST_RUN(test_bump_small);
 
     NEAT_TEST_PRINT_STATS();
 }
